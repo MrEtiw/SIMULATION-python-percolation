af6ceab9ce3d31a732880019ca16a64ba514c9c8
Etienne Boisvenue <etienneboisvenue@gmail.com>
2021-05-09 22:30:13 -0400

    created a common function for getting first and last col clusters

--- clustering.py
+++ clustering.py
@@ -7,23 +7,21 @@ def get_labeled_clusters(grid):
     return labels
 
 
-def get_clusters_first_col(labeled_grid):
-    first_col = labeled_grid[:, 0]
-    first_col_clusters = set(first_col)
-    first_col_clusters.discard(0)
-    return first_col_clusters
 
 
-def get_clusters_last_col(labeled_grid):
-    last_col = labeled_grid[:, -1]
-    last_col_clusters = set(last_col)
-    last_col_clusters.discard(0)
-    return last_col_clusters
+
+def get_clusters_in_a_col(labeled_grid, col_index):
+    col = labeled_grid[:, col_index]
+    clusters_in_col = set(col)
+    clusters_in_col.discard(0)
+    return clusters_in_col
+
+
 
 
 def get_clusters_spanning_from_left_to_right(labeled_grid):
-    first_col_clusters = get_clusters_first_col(labeled_grid)
-    last_col_clusters = get_clusters_last_col(labeled_grid)
+    first_col_clusters = get_clusters_in_a_col(labeled_grid, 0)
+    last_col_clusters = get_clusters_in_a_col(labeled_grid, -1)
     return set.intersection(first_col_clusters, last_col_clusters)
 
 
